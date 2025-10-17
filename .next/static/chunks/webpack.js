efded792f14db52008b760a6db9458b491c7397e
Massab Khan <massab725@gmail.com>
2025-10-18 01:46:37 +0500

    pages

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 		// This function allow to reference async chunks
 /******/ 		__webpack_require__.u = (chunkId) => {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/chunks/" + chunkId + ".js";
+/******/ 			return undefined;
 /******/ 		};
 /******/ 	})();
 /******/ 	
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("68aaa34acebd049d")
+/******/ 		__webpack_require__.h = () => ("c36c71a7965b28bf")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
